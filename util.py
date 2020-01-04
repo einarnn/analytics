b1f5cb4faa421792b42257b8c8cc107b466aa11e
Einar Nilsen-Nygaard <einarnn@gmail.com>
2020-01-04 20:42:35 +0000

    improvements to test data generation

--- util.py
+++ util.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 import random
 
 BASE_SECURITY_ACES = 100
@@ -6,7 +7,7 @@ NUM_POINTS = 1440
 
 def gen_next_data(slot, last_y):
     if slot <= 450:
-        # 0000 to 0730: morning befure people arrive
+        # 0000 to 0730: morning before people arrive
         retval = 100 + int(random.random() * 8 - 4)
     elif slot <= 600:
         # 0730 to 1000: people start arriving
@@ -50,3 +51,57 @@ def gen_random_data_frame(first_index=0):
         y_vals.append(last_y)
     df = pd.DataFrame({'Time': time_vals, 'Security ACEs': y_vals})
     return df
+
+
+def gen_val():
+    last_y = 0
+    slot = 0
+    while True:
+        if slot <= 450:
+            # 0000 to 0730: morning before people arrive
+            last_y = 100 + int(random.random() * 8 - 4)
+        elif slot <= 600:
+            # 0730 to 1000: people start arriving
+            last_y = last_y + int(random.random() * 15 - 5)
+        elif slot <= 720:
+            # 1000 to 1200: a kind of plateau
+            last_y = last_y + int(random.random() * 3 - 1.5)
+        elif slot <= 780:
+            # 1200 to 1300: some people go to lunch
+            last_y = last_y + int(random.random() * 8 - 5)
+        elif slot <= 840:
+            # 1300 to 1400: people come back
+            last_y = last_y + int(random.random() * 8 - 3)
+        elif slot <= 1020:
+            # 1400 to 1700: steadyish state
+            last_y = last_y + int(random.random() * 3 - 1.5)
+        elif slot <= 1260:
+            # 1700 to 2100: people leave office
+            last_y = last_y + int(random.random() * 12 - 8)
+        else:
+            # after 2100: steadyish state
+            last_y = 100 + int(random.random() * 8 - 4)
+        if last_y < 100:
+            last_y = 100
+        slot = (slot + 1) % 1440
+        yield last_y
+
+
+def gen_data(timesteps_per_day=1440, days=1):
+    assert timesteps_per_day <= 1440
+    scaled_ts = None
+    ts = np.arange(0, 1440 * days, dtype=int)
+    values = np.fromiter(gen_val(), count=1440 * days, dtype=int)
+    if timesteps_per_day != 1440:
+        # need to scale data
+        scaled_ts = np.arange(0, 1440 * days, step=(1440/timesteps_per_day), dtype=int)
+        values = np.interp(scaled_ts, ts, values)
+        ts = scaled_ts
+    return np.vstack((ts, values)).T
+
+
+if __name__ == '__main__':
+
+    d = gen_data(timesteps_per_day=1440, days=100)
+    print(d.shape)
+    print(d)
